4f580dcf6c31bbfd28edc20efde2972c6a9e2e78
J1 付雍 <fuy@hz.totyu.com>
2019-09-27 16:16:21 +0800

    Add project files.

---
+++ SetUp/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.IO;
+
+namespace SetUp {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e) {
+            var directory = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            var exe = Path.Combine(directory, "CustomizeFolderTool.exe");
+            if (!File.Exists(exe)) {
+                MessageBox.Show("CustomizeFolderTool.exe不存在");
+                return;
+            }
+            Register.Add(exe + @" ""%1"" /ranfromshell");
+            MessageBox.Show("添加成功");
+        }
+
+        private void BtnDelete_Click(object sender, RoutedEventArgs e) {
+            Register.Delete();
+            MessageBox.Show("删除成功");
+        }
+    }
+}
---
+++ SetUp/Register/Register.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SetUp {
+    class Register {
+        private const string MenuName = "Directory\\shell\\CustomizeFolderTool";
+        private const string Command = "Directory\\shell\\CustomizeFolderTool\\command";
+
+        public static void Add (string command) {
+            RegistryKey regmenu = null;
+            RegistryKey regcmd = null;
+            try {
+                regmenu = Registry.ClassesRoot.CreateSubKey(MenuName);
+                if (regmenu != null) {
+                    regmenu.SetValue("", "自定义文件夹");
+                }
+                regcmd = Registry.ClassesRoot.CreateSubKey(Command);
+                if (regcmd != null)
+                    regcmd.SetValue("", command);
+            } catch (Exception ex) {
+                MessageBox.Show(ex.ToString());
+            } finally {
+                if (regmenu != null)
+                    regmenu.Close();
+                if (regcmd != null)
+                    regcmd.Close();
+            }
+        }
+
+        public static void Delete() {
+            try {
+                RegistryKey reg = Registry.ClassesRoot.OpenSubKey(Command);
+                if (reg != null) {
+                    reg.Close();
+                    Registry.ClassesRoot.DeleteSubKey(Command);
+                }
+                reg = Registry.ClassesRoot.OpenSubKey(MenuName);
+                if (reg != null) {
+                    reg.Close();
+                    Registry.ClassesRoot.DeleteSubKey(MenuName);
+                }
+            } catch (Exception ex) {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+}
