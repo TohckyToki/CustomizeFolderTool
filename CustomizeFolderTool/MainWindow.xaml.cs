c59e81482f628b08fc779af33a6dbf9509302952
付雍 <yuengfu.v@outlook.com>
2020-07-01 21:17:37 +0800

    实现文件夹别名自动刷新，等待后期完善

--- CustomizeFolderTool/MainWindow.xaml.cs
+++ CustomizeFolderTool/MainWindow.xaml.cs
@@ -6,16 +6,22 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.IO;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 
-namespace CustomizeFolderTool {
+namespace CustomizeFolderTool
+{
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window {
+    public partial class MainWindow : Window
+    {
         private string desktop;
 
-        public MainWindow() {
-            if (string.IsNullOrWhiteSpace(App.Path)) {
+        public MainWindow()
+        {
+            if (string.IsNullOrWhiteSpace(App.Path))
+            {
                 this.Close();
                 return;
             }
@@ -24,8 +30,10 @@ namespace CustomizeFolderTool {
             desktop = Path.Combine(App.Path, "desktop.ini");
         }
 
-        private void BtnModify_Click(object sender, RoutedEventArgs e) {
-            if (string.IsNullOrWhiteSpace(txtAlias.Text)) {
+        private void BtnModify_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtAlias.Text))
+            {
                 MessageBox.Show("别名为空");
                 return;
             }
@@ -33,23 +41,53 @@ namespace CustomizeFolderTool {
             sb.AppendLine(Desktop.ShellClassInfo.Name);
             sb.Append($"{Desktop.ShellClassInfo.LocalizedResourceName} = {txtAlias.Text}");
             sb.AppendLine();
-            if (File.Exists(desktop)) {
-                var file = new FileInfo(desktop);
-                file.Attributes = FileAttributes.Normal;
-                file.Refresh();
+            if (File.Exists(desktop))
+            {
+                File.Delete(desktop);
             }
-            File.WriteAllText(desktop, sb.ToString(), Encoding.Default);
+            File.WriteAllText(desktop, sb.ToString(), Encoding.Unicode);
             RefreshDirectory();
+
+
+            LPSHFOLDERCUSTOMSETTINGS FolderSettings = new LPSHFOLDERCUSTOMSETTINGS();
+            FolderSettings.dwMask = 0x10;
+            uint FCS_FORCEWRITE = 0x00000002;
+            var pszPath = App.Path;
+            uint HRESULT = SHGetSetFolderCustomSettings(ref FolderSettings, pszPath, FCS_FORCEWRITE);
             this.Close();
         }
 
-        private void RefreshDirectory() {
+        private void RefreshDirectory()
+        {
             var file = new FileInfo(desktop);
             var directory = new DirectoryInfo(App.Path);
-            directory.Attributes = FileAttributes.ReadOnly | FileAttributes.Directory;
-            directory.Refresh();
-            file.Attributes = FileAttributes.ReadOnly | FileAttributes.Hidden | FileAttributes.Archive;
+            file.Attributes = FileAttributes.Hidden | FileAttributes.System | FileAttributes.Archive;
             file.Refresh();
+            directory.Attributes = FileAttributes.ReadOnly | FileAttributes.System | FileAttributes.Directory;
+            directory.Refresh();
+        }
+
+        [DllImport("Shell32.dll", CharSet = CharSet.Auto)]
+        static extern UInt32 SHGetSetFolderCustomSettings(ref LPSHFOLDERCUSTOMSETTINGS pfcs, string pszPath, UInt32 dwReadWrite);
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+        struct LPSHFOLDERCUSTOMSETTINGS
+        {
+            public UInt32 dwSize;
+            public UInt32 dwMask;
+            public IntPtr pvid;
+            public string pszWebViewTemplate;
+            public UInt32 cchWebViewTemplate;
+            public string pszWebViewTemplateVersion;
+            public string pszInfoTip;
+            public UInt32 cchInfoTip;
+            public IntPtr pclsid;
+            public UInt32 dwFlags;
+            public string pszIconFile;
+            public UInt32 cchIconFile;
+            public int iIconIndex;
+            public string pszLogo;
+            public UInt32 cchLogo;
         }
     }
 }
