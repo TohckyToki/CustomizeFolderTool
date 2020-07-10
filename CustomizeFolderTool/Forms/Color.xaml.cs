b86ca60749b9e985a87a36ce1c94512d56cad3a6
付雍 <yuengfu.v@outlook.com>
2020-07-10 11:03:38 +0800

    新建颜色管理界面

---
+++ CustomizeFolderTool/Forms/Color.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CustomizeFolderTool.Forms {
+    /// <summary>
+    /// Interaction logic for Color.xaml
+    /// </summary>
+    public partial class Color : Window {
+        public Color() {
+            InitializeComponent();
+        }
+
+        private void Grid_Loaded(object sender, RoutedEventArgs e) {
+            var addButton = new Action<Icon, string, int, int>((icon, tooltip, left, top) => {
+                ((Grid)sender).Children.Add(new Button {
+                    Background = new ImageBrush(Imaging.CreateBitmapSourceFromHIcon(new Icon(icon, new System.Drawing.Size(256, 256)).ToBitmap().GetHicon(), Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions())),
+                    Margin = new Thickness(left, top, 0, 0),
+                    ToolTip = tooltip
+                });
+            });
+
+            addButton(Properties.Resources.Default, "默认", 10, 10);
+            addButton(Properties.Resources.Color00, "颜色1", 84, 10);
+            addButton(Properties.Resources.Color01, "颜色2", 158, 10);
+            addButton(Properties.Resources.Color02, "颜色3", 232, 10);
+            addButton(Properties.Resources.Color03, "颜色4", 306, 10);
+            addButton(Properties.Resources.Color04, "颜色5", 10, 84);
+            addButton(Properties.Resources.Color05, "颜色6", 84, 84);
+            addButton(Properties.Resources.Color06, "颜色7", 158, 84);
+            addButton(Properties.Resources.Color07, "颜色8", 232, 84);
+            addButton(Properties.Resources.Color08, "颜色9", 306, 84);
+            addButton(Properties.Resources.Color09, "颜色10", 10, 158);
+            addButton(Properties.Resources.Color10, "颜色11", 84, 158);
+            addButton(Properties.Resources.Color11, "颜色12", 158, 158);
+            addButton(Properties.Resources.Color12, "颜色13", 232, 158);
+            addButton(Properties.Resources.Plus, "自定义", 306, 158);
+        }
+    }
+}
--- CustomizeFolderTool/Properties/Resources.Designer.cs
+++ CustomizeFolderTool/Properties/Resources.Designer.cs
@@ -9,8 +9,9 @@
 //------------------------------------------------------------------------------
 
 namespace CustomizeFolderTool.Properties {
-
-
+    using System;
+    
+    
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -18,33 +19,33 @@ namespace CustomizeFolderTool.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
-
+        
         private static global::System.Resources.ResourceManager resourceMan;
-
+        
         private static global::System.Globalization.CultureInfo resourceCulture;
-
+        
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
         internal Resources() {
         }
-
+        
         /// <summary>
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
-                if ((resourceMan == null)) {
+                if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("CustomizeFolderTool.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
             }
         }
-
+        
         /// <summary>
         ///   Overrides the current thread's CurrentUICulture property for all
         ///   resource lookups using this strongly typed resource class.
@@ -56,6 +57,156 @@ namespace CustomizeFolderTool.Properties {
             }
             set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon Color00 {
+            get {
+                object obj = ResourceManager.GetObject("Color00", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon Color01 {
+            get {
+                object obj = ResourceManager.GetObject("Color01", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon Color02 {
+            get {
+                object obj = ResourceManager.GetObject("Color02", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon Color03 {
+            get {
+                object obj = ResourceManager.GetObject("Color03", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon Color04 {
+            get {
+                object obj = ResourceManager.GetObject("Color04", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon Color05 {
+            get {
+                object obj = ResourceManager.GetObject("Color05", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon Color06 {
+            get {
+                object obj = ResourceManager.GetObject("Color06", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon Color07 {
+            get {
+                object obj = ResourceManager.GetObject("Color07", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon Color08 {
+            get {
+                object obj = ResourceManager.GetObject("Color08", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon Color09 {
+            get {
+                object obj = ResourceManager.GetObject("Color09", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon Color10 {
+            get {
+                object obj = ResourceManager.GetObject("Color10", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon Color11 {
+            get {
+                object obj = ResourceManager.GetObject("Color11", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon Color12 {
+            get {
+                object obj = ResourceManager.GetObject("Color12", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon Default {
+            get {
+                object obj = ResourceManager.GetObject("Default", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon Plus {
+            get {
+                object obj = ResourceManager.GetObject("Plus", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
             }
         }
     }
