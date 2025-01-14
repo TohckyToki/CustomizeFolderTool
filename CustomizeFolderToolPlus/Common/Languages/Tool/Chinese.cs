0e0cfc96b675b851eef48e50b07d48585122c370
YuengFu <yuengfu.v@outlook.com>
2025-01-14 13:30:17 +0800

    Update Chinese.cs

--- CustomizeFolderToolPlus/Common/Languages/Tool/Chinese.cs
+++ CustomizeFolderToolPlus/Common/Languages/Tool/Chinese.cs
@@ -4,51 +4,51 @@ public class Chinese : ILanguage
 {
     public string CodePage => "zh-CN";
 
-    public string ConfirmText => "";
+    public string ConfirmText => "确认";
 
-    public string CancelText => "";
+    public string CancelText => "取消";
 
-    public string AliasTitle => "";
+    public string AliasTitle => "别名";
 
-    public string AliasMessage => "";
+    public string AliasMessage => "请输入文件夹的别名";
 
-    public string IconTitle => "";
+    public string IconTitle => "图标";
 
-    public string CommentTitle => "";
+    public string CommentTitle => "注释";
 
-    public string CommentMessage => "";
+    public string CommentMessage => "请输入文件夹的注释";
 
-    public string IconDefault => "";
+    public string IconDefault => "默认图标";
 
-    public string Icon01 => "";
+    public string Icon01 => "图标1";
 
-    public string Icon02 => "";
+    public string Icon02 => "图标2";
 
-    public string Icon03 => "";
+    public string Icon03 => "图标3";
 
-    public string Icon04 => "";
+    public string Icon04 => "图标4";
 
-    public string Icon05 => "";
+    public string Icon05 => "图标5";
 
-    public string Icon06 => "";
+    public string Icon06 => "图标6";
 
-    public string Icon07 => "";
+    public string Icon07 => "图标7";
 
-    public string Icon08 => "";
+    public string Icon08 => "图标8";
 
-    public string Icon09 => "";
+    public string Icon09 => "图标9";
 
-    public string Icon10 => "";
+    public string Icon10 => "图标10";
 
-    public string Icon11 => "";
+    public string Icon11 => "图标11";
 
-    public string Icon12 => "";
+    public string Icon12 => "图标12";
 
-    public string Icon13 => "";
+    public string Icon13 => "图标13";
 
-    public string IconAdd => "";
+    public string IconAdd => "添加图标";
 
-    public string IconAddTitle => "";
+    public string IconAddTitle => "文件选择";
 
     public string IconAddIcoFilter => "ICO文件";
 
